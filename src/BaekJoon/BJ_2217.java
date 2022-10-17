b83e9b2f4263b13f9869b47c66d874d9fb9f98c3
Sol Kim <101856058+Blessole@users.noreply.github.com>
2022-10-17 23:21:31 +0900

    221017 그리디 알고리즘 - 로프

---
+++ src/BaekJoon/BJ_2217.java
@@ -0,0 +1,28 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+// 로프
+public class BJ_2217 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        long max = 0;
+        for (int i=N-1; i>=0; i--){
+            arr[i] = arr[i]*(N-i);
+            if (max < arr[i]) max = arr[i];
+        }
+        System.out.println(max);
+    }
+}
